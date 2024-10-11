beac87335a944fef63f627b3b2aab2ab8e937381
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-10-11 15:23:45 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241011/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 241011/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,9 +2,9 @@ def check_arr(a, b):
     for i in range(len(a)):
         if (b[0] == a[i]):
             for j in range(len(b)):
-                if i + j <len(a):
-                    if(b[j]!=a[i+j]):
-                        return False
+                
+                if(b[j]!=a[i+j]):
+                    return False
     return True
 
 n1, n2 = map(int, input().split())
