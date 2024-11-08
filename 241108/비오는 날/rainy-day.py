fb6f7b19c2aef54b98ea6f49d2aa0d0366a5072d
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-11-08 09:34:51 +0900

    Title: 비오는 날, Time: 46ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241108/비오는 날/rainy-day.py
+++ 241108/비오는 날/rainy-day.py
@@ -11,7 +11,11 @@ for i in range(n):
     d1, d2, w = tuple(input().split())
     info.append(weather(d1, d2, w))
 
+minyear = 2100
+mininfo = info[0]
 for i in range(n):
-    if info[i].wea == 'Rain':
-        print(info[i].date, info[i].day, info[i].wea)
-        break
+    
+    if info[i].wea == 'Rain' and int(info[i].date[0:4]) < minyear:
+        minyear = int(info[i].date[0:4])
+        mininfo = info[i]
+print(mininfo.date, mininfo.day, mininfo.wea)
