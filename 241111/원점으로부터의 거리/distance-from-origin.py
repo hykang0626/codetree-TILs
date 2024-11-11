3c3e18b1c8f01a72682072bd43d65e2f106692de
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-11-11 15:26:36 +0900

    Title: 원점으로부터의 거리, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241111/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,17 @@
+class dot:
+    def __init__ (self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+dots = []
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+    dots.append(dot(x, y, i+1))
+
+dots.sort(key = lambda dot : dot.x**2 + dot.y**2)
+
+for d in dots:
+    print(d.num)
