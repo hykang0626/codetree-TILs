adff347d0e7931b197825f682baa4d1f27bbad20
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-11-12 14:19:29 +0900

    Title: 그 요일은, Time: 45ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241112/그 요일은/the-day-of-the-day.py
+++ 241112/그 요일은/the-day-of-the-day.py
@@ -9,17 +9,26 @@ for i in range(m1-1):
     start+=days[i]
 
 # start % 7 ->월요일인 인덱스, start %7 -1 = 월요일인덱스와의 차이
+find = start % 7
+# print(find)
+for i in range(7):
+    if week_of_day[i] == check_day:
+        check_idx = i - find
+        # print(check_idx)
 
 finish = d2
 for i in range(m2-1):
     finish += days[i]
 
-sub_day = finish - start +1
-count = sub_day // 7 
-if sub_day % 7 > 0:
-    # print(sub_day % 7)
-    for i in range(sub_day % 7 + 1):
-        if week_of_day[i] == check_day:
-            count+=1
+start_check = start // 7
+if start % 7 >= check_idx:
+    start_check += 1
 
-print(count)
+fin_check = finish // 7
+# print(finish)
+if finish % 7 >= check_idx:
+    fin_check += 1
+
+# print(start_check, fin_check)
+print(fin_check - start_check)
+
