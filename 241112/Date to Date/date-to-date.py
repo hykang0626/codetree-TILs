1aba9873f6105c86bd503256b2c587d87d5ad0b6
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-11-12 10:42:17 +0900

    Title: Date to Date, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241112/Date to Date/date-to-date.py
+++ 241112/Date to Date/date-to-date.py
@@ -2,7 +2,10 @@ days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 m1, d1, m2, d2 = map(int, input().split())
 
 sum = days[m1-1] - d1 + 1 + d2
-for i in range (m1+1, m2):
-    sum += days[i]
+if m1 == m2 and d1 == d2:
+    sum = 1
+else:
+    for i in range (m1, m2-1):
+        sum += days[i]
 
 print(sum)
