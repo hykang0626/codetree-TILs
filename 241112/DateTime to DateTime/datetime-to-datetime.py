8669e9e70c3e89081e769ffbdd8924ce159c76b3
hykang0626 <60470098+hykang0626@users.noreply.github.com>
2024-11-12 11:09:25 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241112/DateTime to DateTime/datetime-to-datetime.py
+++ 241112/DateTime to DateTime/datetime-to-datetime.py
@@ -3,4 +3,7 @@ a, b, c = map(int, input().split())
 start = 11 * 60 * 24 + 11 * 60 + 11
 finish =  a * 60 * 24 + b * 60 + c
 
-print(finish - start)
+if finish - start < 0:
+    print(-1)
+else:
+    print(finish - start)
